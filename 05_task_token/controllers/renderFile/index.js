c8651b00f4a26b962e613324e1369483fc158fb5
IrynaOst <52861410+IrynaOst@users.noreply.github.com>
2019-10-19 02:39:18 +0300

    fixed needless files

--- 05_task_token/controllers/renderFile/index.js
+++ 05_task_token/controllers/renderFile/index.js
@@ -2,18 +2,15 @@ const main = require('./main');
 const auth = require('./auth');
 const registerUser = require('./registerUser');
 const profileUser = require('./profileUser');
-const updateUser = require('./updateUser');
 const createHouse = require('./createHouse');
 const profileHouse = require('./profileHouse');
-const updateHouse = require('./updateHouse');
+
 
 module.exports = {
     main,
     auth,
     registerUser,
     profileUser,
-    updateUser,
     createHouse,
     profileHouse,
-    updateHouse
 }
