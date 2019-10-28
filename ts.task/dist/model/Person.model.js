bb6b0f1b9fdb4d6cfdaf09bdaaeeb87e604a019b
IrynaOst <52861410+IrynaOst@users.noreply.github.com>
2019-10-28 19:05:23 +0200

    delete unnecessary files

--- ts.task/dist/model/Person.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Person {
-    constructor(personHeight, personWeight) {
-        this.height = personHeight;
-        this.weight = personWeight;
-    }
-}
-exports.Person = Person;
